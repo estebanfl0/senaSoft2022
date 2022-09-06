4ba7f44d323ff6d5ce719135ef2fda5103aa4bd1
Juan Jose Valencia <juan.valencia927@misena.edu.co>
2022-09-06 17:07:34 -0500

    Update cardscontroller.php
    add method seedCards
    add method getAllCards

--- MVC/controllers/cardscontroller.php
+++ MVC/controllers/cardscontroller.php
@@ -1,7 +1,19 @@
 <?php 
+require_once('models/card.php');
 class cardscontroller{
-    public function index(){
-        // mostrar todas las tarjetas
-        
+    public function seedCards(){
+        // Generar todas las cartas
+        if(isset($_GET['seed'])){
+            require_once('dataBase/insertcards.php');
+            $create = seedCards();
+            if($create){
+                header("location:".baseController."controller=player&action=index");
+            }
+        }
+    }
+    // metodo para regresar todas las cartas
+    public function getAllCards(){  
+        $cards = utils::returnAllCards();
+        return $cards;
     }
 }
