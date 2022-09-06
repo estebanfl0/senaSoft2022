7415a2488000645ba6ad7ce5e42141e853559429
Juan Jose Valencia <juan.valencia927@misena.edu.co>
2022-09-06 13:39:03 -0500

    front controller
    make front controller

---
+++ MVC/index.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+require_once('autoload.php');
+require_once('config/db.php');
+require_once('config/parameters.php');
+
+if(isset($_GET['controller'])){
+    $controller_name = $_GET['controller'].'controller';
+}
+elseif(!isset($_GET['controller']) && !isset($_GET['action'])){
+    $controller_name = controler_default;
+
+}
+else {
+    $erro = true;
+    return $error;
+}
+if(class_exists($controller_name)){
+    $controller = new $controller_name();
+    if(isset($_GET['action']) && method_exists($controller,$_GET['action'])){
+        $action =  $_GET['action'];
+        $controller->$action();
+    }elseif(!isset($_GET['controller']) && !isset($_GET['action'])){
+        $default = action_default;
+        $controller->$default();
+    }
+    else{
+        $erro = true;
+        return $error;
+    }
+
+}else {
+    $erro = true;
+    return $error;
+}
