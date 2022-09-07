ca027532d12317e43e7b6300e7efebd029839436
Juan Jose Valencia <juan.valencia927@misena.edu.co>
2022-09-07 17:32:42 -0500

    Update player.php

--- MVC/models/player.php
+++ MVC/models/player.php
@@ -37,45 +37,44 @@ class player{
     public function createPlayer($decks){
         $db = dataBase::conexion();
         $name = $this->getName();
-        utils::alterSessionDecks($decks,0);
-        $this->setDeck_id($_SESSION['idDeck']);
+        
+        // utils::alterSessionDecks($decks);
+        // $this->setDeck_id($_SESSION['idDeck'][0]->id);
         $deck_id = $this->getDeck_id();
-        $sqlvalidate = "SELECT * FROM players WHERE deck_id= {$_SESSION['idDeck']}";
-        $validate = $db->query($sqlvalidate);
+        // $sqlvalidate = "SELECT * FROM players WHERE deck_id= {$this->getDeck_id()}";
+        // $validate = $db->query($sqlvalidate);
 
-        if(empty($validate)){
+        // if(empty($validate)){}
 
-            $sql = "INSERT INTO players VALUES (null,'$name',$deck_id)";
-            $save = $this->db->query($sql);
-            $result = false;
-            dataBase::close($db);
-
-            if($save){
-                $result=true;
-            }
-            return $result;    
-        }else {
-            switch($_SESSION['idDeck']){
-                case 1:
-                    utils::alterSessionDecks($decks,1);
-                    break;
-                case 2:
-                    utils::alterSessionDecks($decks,2);
-                    break;
-                case 3:
-                    utils::alterSessionDecks($decks,3);
-                    break;
-                case 4:
-                    utils::alterSessionDecks($decks,4);
-                    break;
-                case 5:
-                    utils::alterSessionDecks($decks,5);
-                    break;
-                case 6:
-                    utils::alterSessionDecks($decks,6);
-                    break;
-            }
-        $this->createPlayer($decks);
+        $sql = "INSERT INTO players VALUES (null,'$name',$deck_id)";
+        $save = $db->query($sql);
+        $result = false;
+        dataBase::close($db);
+        if($save){
+            $result=true;
         }
+        // }else {
+        //     switch($_SESSION['idDeck']){
+        //         case 1:
+        //             utils::alterSessionDecks($decks,1);
+        //             break;
+        //         case 2:
+        //             utils::alterSessionDecks($decks,2);
+        //             break;
+        //         case 3:
+        //             utils::alterSessionDecks($decks,3);
+        //             break;
+        //         case 4:
+        //             utils::alterSessionDecks($decks,4);
+        //             break;
+        //         case 5:
+        //             utils::alterSessionDecks($decks,5);
+        //             break;
+        //         case 6:
+        //             utils::alterSessionDecks($decks,6);
+        //             break;
+        //     }
+        return $result;    
+        
     }
 } //end class
