40f2540b720a547c51375ec04d88dc111614e638
Juan Jose Valencia <juan.valencia927@misena.edu.co>
2022-09-07 15:15:43 -0500

    Update playercontroller.php
    -add method for generte deckCards to game
    -first try to generate player

--- MVC/controllers/playercontroller.php
+++ MVC/controllers/playercontroller.php
@@ -1,19 +1,27 @@
 <?php 
 require_once('models/player.php');
 class playercontroller{
-    public function createPlayer(){
-        if(isset($_GET['name']) && isset($_GET['numPlayers'])){
-            $name = $_GET['name'];
-            $numplayers = $_GET['numPlayers'];
-            // desde aqui se maneja el controlador de los mazos
-            $ctrDeckcards = new deckCardscontroller();
-            $ctrDeckcards->saveDeck($numplayers);
-            
 
-            // desde aqui se maneja el controlador de jugador
+    public function createDecksgame(){
+        if(isset($_GET['numPlayers'])){
+            utils::generateAllDecks($_GET['numPlayers']);
+            // header("location:".baseController."controller=player&action=createPlayer&name=player");
+        }
+        
+    }
+    public function createPlayer(){
+        if(isset($_GET['name'])){
+            $name = $_GET['name'];
+            $deckCards = new deckCard();
+            $deckCards = $deckCards->getAll();
             $playermodel = new player();
             $playermodel->setName($name);
-            $playermodel->createPlayer();
+            $playermodel->setDeck_id($deckCards);
+            $result = $playermodel->createPlayer($deckCards);
+
+            if($result){
+                echo "conseguido";
+            }
 
         }
     }
