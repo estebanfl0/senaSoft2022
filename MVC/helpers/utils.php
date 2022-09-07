ce59c3544363b7b1139166bc5d4baafdd4a830d4
Juan Jose Valencia <juan.valencia927@misena.edu.co>
2022-09-07 12:01:04 -0500

    Update utils.php

--- MVC/helpers/utils.php
+++ MVC/helpers/utils.php
@@ -10,4 +10,24 @@ class utils{
         }
         return $cards;
     }
-}
+
+    public static function maxcardDecks($numplayers){
+        $result = 0;
+        $res = 32/$numplayers;
+        $result = floor($res);
+        return $result;
+    }
+    public static function generateDeck($numCardsDeck,$cards){
+        $count = 0;
+        $arrayplayer = array();
+        while($count<$numCardsDeck) {
+            $randnum = rand(0,31);
+            if(isset($cards[$randnum])){
+                array_push($arrayplayer,$cards[$randnum]);
+                unset($cards[$randnum]);
+                $count++;
+            };
+        };
+        return $arrayplayer;
+    }
+}// end class
