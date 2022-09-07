54f6a984ab243c3387e2fa2634f43fedfc78a002
Juan Jose Valencia <juan.valencia927@misena.edu.co>
2022-09-07 09:10:50 -0500

    Update deckCard.php
    add new functions

--- MVC/models/deckCard.php
+++ MVC/models/deckCard.php
@@ -4,6 +4,11 @@ class deckCard{
     // se generan los atributos del modelo
     private $id;
     private $deck;
+    private $db;
+
+    public function __construct(){
+        $this->db = dataBase::conexion();
+    }
 
     // geter and setter
     public function getId(){
@@ -21,7 +26,31 @@ class deckCard{
     public function setDeck($deck){
         $this->deck = $deck;
     }
-    public function save($cards){
-        
+    public function save($deck){
+        $deck = json_encode($deck);
+        $sql = "INSERT INTO deckcards VALUES (null,'$deck')";        
+        $save = $this->db->query($sql);
+        $result = false;
+        if($save){
+            $result=true;
+        }
+        return $result;
     }
-}
+
+    public function getOne($id){
+        $sql = "SELECT * FROM deckcards WHERE id = $id";        
+        $answer = $this->db->query($sql);
+        $result = mysqli_fetch_object($answer);
+        return $result;
+    }
+    
+    public function getAll(){
+        $sql = "SELECT * FROM deckcards";
+        $answer = $this->db->query($sql);
+        $result = array();
+        while($res = mysqli_fetch_object($answer)){
+            array_push($result,$res);
+        }
+        return $result;
+    }
+}//end class
