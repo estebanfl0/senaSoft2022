b97018ad19f725ac5dd378baeadfcfe08d3cfcff
Juan Jose Valencia <juan.valencia927@misena.edu.co>
2022-09-07 12:01:11 -0500

    Update deckCardscontroller.php

--- MVC/controllers/deckCardscontroller.php
+++ MVC/controllers/deckCardscontroller.php
@@ -1,61 +1,23 @@
 <?php
 require_once('models/deckCard.php');
 class deckCardscontroller{
-    public function saveDeck(){
-        if($_POST){
-            $numplayers = $_POST['numPLayers'];
-            $numCards = 32;
-            switch($numplayers){
-                case 2:
-                    $numCardsDeck = $numCards/$numplayers;
-                    break;
-                case 3:
-                    $numCardsDeck = $numCards/$numplayers;
-                    break;
-                case 4:
-                    $numCardsDeck = $numCards/$numplayers;
-                    break;
-                case 5:
-                    $numCardsDeck = $numCards/$numplayers;
-                    break;
-                case 6:
-                    $numCardsDeck = $numCards/$numplayers;
-                    break;
-                case 7:
-                    $numCardsDeck = $numCards/$numplayers;
-                    break;
-        
-            }
-            $carcontrol = new cardscontroller();
-            $cards=$carcontrol->getAllCards();
-            $deckCards = array();
-            $count = 0;
-            while($count<$numCardsDeck) {
-                $randnum = rand(0,31);
-                if(isset($cards[$randnum])){
-                    array_push($deckCards,$cards[$randnum]);
-                    unset($cards[$randnum]);
-                    $count++;
-                };
-            };
-            $deckCard = array_chunk($deckCards,(sizeof($deckCards)/$numplayers),true);
-            var_dump($deckCard);
-            die();
-            $deckCard01 = $deckCard[0];
-            $deckCard02 = $deckCard[1];
-            
-            
-            // // se guarda 
-            // $deck = new deckCard();
-            // $result = $deck->save($deckCard01);
-            // $deck->save($deckCard02);
-            // if($result){
-            //     header("location: index.php");
-            // }else {
-            //     echo "error en la ejecucion sql";
-            // }
+    public function saveDeck($numplayers){
+            $numplayers = $numplayers;
+            $numCardsDeck = utils::maxcardDecks($numplayers);     
+            $cardcontrol = new cardscontroller();
+            $cards=$cardcontrol->getAllCards();
+            $id= $_GET['id'];
 
-        }
+            $array = utils::generateDeck($numCardsDeck,$cards);
+
+            // se guarda el mazo de cartas para el juagdor
+            $deck = new deckCard();
+            $result = $deck->save($array);
+            if($result){
+                header("location: index.php");
+            }else {
+                echo "error en la ejecucion sql";
+            }
 
     }
 
@@ -64,5 +26,9 @@ class deckCardscontroller{
         $decksplay = $decks->getAll();
         var_dump($decksplay);
         return $decksplay;
+    }
+
+    public function getAllRand(){
+        
     }
 }//end class
