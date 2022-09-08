e5a07652eb73adf2ae0f4a0d2689a07dc670ed2c
estebanfl0 <esteban.florez3@misena.edu.co>
2022-09-08 10:29:54 -0500

    Update playercontroller.php

--- MVC/controllers/playercontroller.php
+++ MVC/controllers/playercontroller.php
@@ -1,7 +1,20 @@
 <?php 
 require_once('models/player.php');
 class playercontroller{
+    // Metodos para requerrir vistas
+    public function index() {
+        require_once('views/player/index.php');
+    }
+    public function cardTemplatee() {
+        require_once('template/cardT.php');
+    }
+    public function placeGame() {
+        require_once('views/placegame/lacegame.php');
+    }
 
+
+
+    // Metodos para generar mazos y jugadores 
     public function createDecksgame(){
         if(isset($_GET['numPlayers'])){
             utils::generateAllDecks($_GET['numPlayers']);
