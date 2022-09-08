ab4f93e7fa151147b70675566b09ccf1e567584c
estebanfl0 <esteban.florez3@misena.edu.co>
2022-09-08 10:28:43 -0500

    Update parameters.php

--- MVC/config/parameters.php
+++ MVC/config/parameters.php
@@ -1,5 +1,5 @@
 <?php
-define("base_url","http://localhost:3000");
+define("base_url","http://localhost:3000/");
 define("controler_default","playercontroller");
 define("action_default","index");
 define("baseController","index.php/?");
